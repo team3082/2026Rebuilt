7687374527f99760852a013f411d1eba87734f69
team3082-user <minnetonka3082@gmail.om>
2026-02-05 19:01:34 -0600

    super cool changes idk

--- src/main/java/frc/robot/subsystems/visualizer/ShooterVisualizer.java
+++ src/main/java/frc/robot/subsystems/visualizer/ShooterVisualizer.java
@@ -1,58 +1,58 @@
 package frc.robot.subsystems.visualizer;
 
-
-import java.util.PrimitiveIterator;
-
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.Constants;
 import frc.robot.Telemetry;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Turret;
 
 public class ShooterVisualizer {
-private static MechanismRoot2d hoodRoot;
-private static MechanismLigament2d hoodLig;
+    private static MechanismRoot2d hoodRoot;
+    private static MechanismLigament2d hoodLig;
 
-private static MechanismLigament2d flywheelLig1;
-private static MechanismLigament2d flywheelLig2;
-private static MechanismLigament2d flywheelLig3;
-private static MechanismLigament2d flywheelLig4;
+    private static WheelMech mech;
 
-private static MechanismRoot2d turretRoot;
-private static MechanismLigament2d turretLig;
+    private static MechanismRoot2d turretRoot;
+    private static MechanismLigament2d turretLig;
 
+    private static MechanismRoot2d turretSideRoot;
+    private static MechanismLigament2d turretFrontLig, turretBackLig;
 
+    private static double hoodAngle;
+    private static double flywheelAngle;
+    private static double flywheelSpeed;
 
-private static double hoodAngle;
-private static double flywheelAngle;
-private static double flywheelSpeed;
+    private static double angle;
 
-
-    
     public static void init(){
         
-        hoodRoot = Telemetry.subsystemView.getRoot("shooterRoot", 1, 1);
+        hoodRoot = Telemetry.subsystemViewSide.getRoot("shooterRoot", 1, 1);
         hoodLig = hoodRoot.append(new MechanismLigament2d("hoodLig", 2, 0));
-        flywheelLig1 = hoodLig.append(new MechanismLigament2d("flywheelLig1", .5, 0));
-        flywheelLig2 = hoodLig.append(new MechanismLigament2d("flywheelLig2", .5, 90));
-        flywheelLig3 = hoodLig.append(new MechanismLigament2d("flywheelLig3", .5, 180));
-        flywheelLig4 = hoodLig.append(new MechanismLigament2d("flywheelLig4", .5, 270));
+        mech = new WheelMech("Fly Wheel", hoodLig, 9, .6, 5,  new Color8Bit(Color.kAliceBlue));
 
-        turretRoot = Telemetry.subsystemView.getRoot("turretRoot", 1, 3);
+        turretSideRoot = Telemetry.subsystemViewSide.getRoot("TurretRight", 5, 2  );
+        turretFrontLig = turretSideRoot.append(new MechanismLigament2d("asdsd", 1, 0));
+        turretBackLig = turretSideRoot.append(new MechanismLigament2d("asdsdd", 1, 180));
+        turretBackLig.setColor(new Color8Bit(Color.kBlanchedAlmond));
+
+        turretRoot = Telemetry.subsystemViewTop.getRoot("turretRoot", 1, 3);
         turretLig = turretRoot.append(new MechanismLigament2d("turretLig", 1, 0));
     }
 
     public static void update(){
         hoodAngle = Shooter.getAngle();
         flywheelSpeed = Shooter.getVelocity();
+        angle += 0.05;
     
-        hoodLig.setAngle(hoodAngle);
-        flywheelAngle += flywheelSpeed * 0.1;
-        flywheelLig1.setAngle(flywheelAngle);
-        flywheelLig2.setAngle(flywheelAngle + 90);
-        flywheelLig3.setAngle(flywheelAngle + 180);
-        flywheelLig4.setAngle(flywheelAngle + 270);
+        hoodLig.setAngle(hoodAngle * 180/Math.PI);
+        mech.update(flywheelSpeed*.01);
 
-        turretLig.setAngle(Turret.getAngle());
+        Turret.setAngle(angle / Constants.RAD_TO_DEGREES);
+        turretLig.setAngle(Turret.getAngle() * Constants.RAD_TO_DEGREES);
+        turretBackLig.setLength(Math.cos(Turret.getAngle()));
+        turretFrontLig.setLength(Math.cos(-Turret.getAngle()));
     }
 }
